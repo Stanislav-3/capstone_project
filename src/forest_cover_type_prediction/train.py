8d2297f7c2cf8b0f4d88b7999b927b73f36ccef2
Stanislav-3 <61240903+Stanislav-3@users.noreply.github.com>
2022-05-09 19:48:19 +0300

    Register CLI

--- src/forest_cover_type_prediction/train.py
+++ src/forest_cover_type_prediction/train.py
@@ -5,11 +5,11 @@ import pandas as pd
 
 @click.command()
 @click.option(
-     "-s",
-     "--source",
-     default="data/sampleSubmission.csv",
-     type=click.Path(exists=True, dir_okay=False, path_type=Path)
+    "-s",
+    "--source",
+    default="data/sampleSubmission.csv",
+    type=click.Path(exists=True, dir_okay=False, path_type=Path)
 )
-def train(dataset_path: Path) -> None:
-    dataset = pd.read_csv(dataset_path)
+def train(source: Path) -> None:
+    dataset = pd.read_csv(source)
     click.echo(f"Dataset shape: {dataset.shape}.")
