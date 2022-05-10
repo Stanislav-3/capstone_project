f9ff4b0527876b0a69fc2ecbd97d2b9507e7acac
Stanislav-3 <61240903+Stanislav-3@users.noreply.github.com>
2022-05-10 11:33:25 +0300

    created simple get_dataset function

--- src/forest_cover_type_prediction/data.py
+++ src/forest_cover_type_prediction/data.py
@@ -3,4 +3,10 @@ from pathlib import Path
 
 
 def get_dataset(csv_path: Path) -> Tuple[pd.DataFrame, pd.Series]:
-    pass
+    data = pd.read_csv('data/train.csv')
+
+    X = data.drop(['Id'], axis=1).values
+    y = data['Cover_Type']
+
+    return X, y
+
