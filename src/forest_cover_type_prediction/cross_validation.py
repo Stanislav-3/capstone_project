b7d66020522a75d303d58629e15011ec4fcd5f66
Stanislav-3 <61240903+Stanislav-3@users.noreply.github.com>
2022-05-10 13:32:46 +0300

    Added cross validation

---
+++ src/forest_cover_type_prediction/cross_validation.py
@@ -0,0 +1,26 @@
+from sklearn.model_selection import KFold
+from sklearn.metrics import accuracy_score, recall_score, precision_score
+
+
+def cross_validate(X, y, model, n_splits=2):
+    scores = {
+        'accuracy_score': 0,
+        'precision_score': 0,
+        'recall_score': 0
+    }
+
+    kf = KFold(n_splits=n_splits)
+
+    for train_index, test_index in kf.split(X):
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+
+        model.fit(X_train, y_train)
+
+        prediction = model.predict(X_test)
+
+        scores['accuracy_score'] += accuracy_score(prediction, y_test)
+        scores['precision_score_micro'] += precision_score(prediction, y_test, average='micro')
+        scores['recall_score'] += recall_score(prediction, y_test, average='micro')
+
+    return {key: value / n_splits for key, value in scores.items()}
--- src/forest_cover_type_prediction/train.py
+++ src/forest_cover_type_prediction/train.py
@@ -2,11 +2,9 @@ from pathlib import Path
 import click
 import pandas as pd
 
-from sklearn.model_selection import KFold
-from sklearn.metrics import accuracy_score, recall_score, precision_score
-
 from src.forest_cover_type_prediction.data import get_dataset
 from src.forest_cover_type_prediction.pipeline import create_pipeline
+from src.forest_cover_type_prediction.cross_validation import cross_validate
 
 
 @click.command()
@@ -27,23 +25,8 @@ def train(source: Path) -> None:
                                  'max_iter': 10e4
                              })
 
-    model1.fit(X, y)
-    model2.fit(X, y)
-
-    prediction1 = model1.predict(X)
-    prediction2 = model2.predict(X)
-
-    model1_scores = {
-        'accuracy_score': accuracy_score(prediction1, y),
-        'precision_score': precision_score(prediction1, y, average='micro'),
-        'recall_score': recall_score(prediction1, y, average='micro')
-    }
-
-    model2_scores = {
-        'accuracy_score': accuracy_score(prediction2, y),
-        'precision_score': precision_score(prediction2, y, average='micro'),
-        'recall_score': recall_score(prediction2, y, average='micro')
-    }
+    model1_scores = cross_validate(X, y, model1, n_splits=3)
+    model2_scores = cross_validate(X, y, model2, n_splits=3)
 
     click.echo(model1_scores)
     click.echo(model2_scores)
