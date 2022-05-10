bb83757008adf5626e85210f5bcbdd734dc89ff7
Stanislav-3 <61240903+Stanislav-3@users.noreply.github.com>
2022-05-10 11:26:06 +0300

    improved pipeline

--- src/forest_cover_type_prediction/pipeline.py
+++ src/forest_cover_type_prediction/pipeline.py
@@ -4,6 +4,12 @@ from sklearn.preprocessing import StandardScaler
 
 def create_pipeline(use_scaler: bool = False, model_type: str = 'knn', hyperparams: dict = {}) -> Pipeline:
     steps = []
+    if use_scaler:
+        steps.append(("scaler", StandardScaler()))
+    if model_type == 'knn':
+        model = KNeighborsClassifier(**hyperparams)
+    else:
+        model = LogisticRegression(**hyperparams)
 
     steps.append(("classifier", model))
 
